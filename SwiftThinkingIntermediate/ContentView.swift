a4b760c5fff13557a2c6ae2659b49c9a8609fb4a
Kyungyun Lee, IOS dev <leeky16498@gmail.com>
2022-03-06 13:12:59 +0000

    Initial Commit

---
+++ SwiftThinkingIntermediate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftThinkingIntermediate
+//
+//  Created by Kyungyun Lee on 06/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftThinkingIntermediate/SwiftThinkingIntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftThinkingIntermediateApp.swift
+//  SwiftThinkingIntermediate
+//
+//  Created by Kyungyun Lee on 06/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftThinkingIntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
